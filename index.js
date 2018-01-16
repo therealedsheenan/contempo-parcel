10ec5f000adf0adc0dce958ee67b7f716e5c6d05
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-01-16 14:33:01 +0800

    partial file

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-console.log("hello world");
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+// main scss file
+import './styles/main.scss';
+
+const App  = () => (
+  <div className="App">
+    <img className="App-Logo" src={logo} alt="React Logo" />
+    <h1 className="App-Title">Hello Parcel x React</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// Hot Module Replacement
+if (module.hot) {
+  module.hot.accept();
+}
