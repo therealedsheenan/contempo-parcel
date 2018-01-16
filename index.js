bc83f56135b41ed3b4362f2ce9836afdf874a284
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-01-16 15:00:21 +0800

    index

--- index.js
+++ index.js
@@ -4,22 +4,28 @@ import { BrowserRouter } from 'react-router-dom';
 import { AppContainer } from 'react-hot-loader';
 
 // components
-import App from './App';
+// import App from './App';
 
 // main scss file
 import './styles/main.scss';
 
 const $root = document.getElementById('root');
 
-if (process.env.NODE_ENV === 'production') {
-  // production setup
-  ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    $root
-  );
-}
+// if (process.env.NODE_ENV === 'production') {
+//   // production setup
+//   ReactDOM.render(
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>,
+//     $root
+//   );
+// }
+
+const App = () => {
+  return (
+    <div>this is a test.</div>
+  )
+};
 
 // development setup with HMR
 const render = Component => {
@@ -36,8 +42,8 @@ const render = Component => {
 render(App);
 
 // Hot Module Replacement API
-if (module.hot) {
-  module.hot.accept('./App', () => {
-    render(App);
-  });
-}
+// if (module.hot) {
+//   module.hot.accept('./App', () => {
+//     render(App);
+//   });
+// }
