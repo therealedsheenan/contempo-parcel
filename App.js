27bbb41f96bee9cd40ca1b8a29ded203944cc5cc
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-01-16 14:54:49 +0800

    hot module

---
+++ App.js
@@ -0,0 +1,5 @@
+export default () => (
+  <div className="App">
+    <h1 className="App-Title">Hello Contempo-parcel</h1>
+  </div>
+);
--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { AppContainer } from 'react-hot-loader';
+
+// components
+import App from './App';
 
 // main scss file
 import './styles/main.scss';
 
-const App = () => (
-  <div className="App">
-    <h1 className="App-Title">Hello Contempo-parcel</h1>
-  </div>
-);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (process.env.NODE_ENV === 'production') {
+  // production setup
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+  );
+}
 
-// Hot Module Replacement
+// development setup with HMR
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <BrowserRouter>
+        <Component />
+      </BrowserRouter>
+    </AppContainer>,
+    document.getElementById('root')
+  );
+};
+
+render(App);
+
+// Hot Module Replacement API
 if (module.hot) {
-  module.hot.accept();
+  module.hot.accept('./App', () => {
+    render(App);
+  });
 }
