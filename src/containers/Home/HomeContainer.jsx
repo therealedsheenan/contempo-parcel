82fe23ed06d575815bf6ebdad9b16396e7c099c5
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-01-17 11:20:49 +0800

    remove file

--- src/containers/Home/HomeContainer.jsx
+++ src/containers/Home/HomeContainer.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import GreetingComponent from '../../components/Greeting/GreetingComponent';
-
 export default () => (
   <div>
     <h1>Welcome to contempo-parcel.</h1>
