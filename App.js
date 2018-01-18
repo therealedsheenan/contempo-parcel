64e814ec15ab44f70459275fc4736c34710f2aa8
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-01-18 11:11:38 +0800

    update App markup

--- App.js
+++ App.js
@@ -7,9 +7,7 @@ import store from './src/config/store';
 
 export default () => (
   <Provider store={store}>
-    <div className="App">
-      {routes()}
-    </div>
+    {routes()}
   </Provider>
 );
 
